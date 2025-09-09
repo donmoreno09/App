c7c74da1b4dd7a9887148aaad73ccc3a12537203
valerio crispini <valerio.crispini.ext@fincantieri.it>
2025-09-09 15:23:33 +0200

    #576 refactor: moved target_include_directories from Logger's CMakeLists to root's

--- App/main.cpp
+++ App/main.cpp
@@ -1,7 +1,7 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QDir>
-#include <Logger/app_logger.h>
+#include <App/Logger/app_logger.h>
 
 int main(int argc, char *argv[])
 {
