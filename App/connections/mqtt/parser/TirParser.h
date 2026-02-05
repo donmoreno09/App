b9462d2b13c6b549076ad441b5b6364511ac26ae
Sherwin Arellano <sherwin.arellano.ext@fincantieri.it>
2026-02-05 09:18:28 +0100

    #1187 fix: Invert TIR coordinates

--- App/connections/mqtt/parser/TirParser.h
+++ App/connections/mqtt/parser/TirParser.h
@@ -29,7 +29,7 @@ public:
             tir.uidForHistory = tirVal["operationCode"].toString();
             tir.name = tirVal["operationCode"].toString();
             tir.operationCode = tirVal["operationCode"].toString();
-            tir.pos = parseCoordinateArray(tirVal["pos"].toArray(), false);
+            tir.pos = parseCoordinateArray(tirVal["pos"].toArray(), true);
             tir.sourceName = "Tir";
             tir.cog = tirVal["cog"].toDouble();
             tir.time = tirVal["time"].toInt();
